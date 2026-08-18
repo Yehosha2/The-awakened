1519506c960514f02cbfa3ee635716b74d5710fc
Yehosha2 <tacoreon123@icloud.com>
2026-08-18 14:12:31 -0400

    Add button click event to reveal message

---
+++ script.js
@@ -0,0 +1,13 @@
+const button = document.getElementById("reveal");
+
+const message = document.getElementById("message");
+
+button.addEventListener("click", function () {
+
+  message.textContent =
+    "The first door is already open.";
+
+  button.textContent =
+    "THE JOURNEY HAS BEGUN";
+
+});
