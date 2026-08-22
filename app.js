a23b8f83089de9c448d7e2a32cd360cde71d4e0a
Yehosha2 <tacoreon123@icloud.com>
2026-08-22 14:34:56 -0400

    Update app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-// app.js
-// THE AWAKENED — Book One
-// Home Page JavaScript
-
-document.addEventListener("DOMContentLoaded", function () {
-
-  console.log("THE AWAKENED home page loaded.");
-
-});
