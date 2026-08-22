be27a57fe7e2fe44c183c9618313104e22319177
Yehosha2 <tacoreon123@icloud.com>
2026-08-21 22:37:31 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 let currentPage = 0;
 
 // Get all book pages
-const pages = document.querySelectorAll(".book-page");
+const pages = document.querySelectorAll(".page");
 
 // Total pages
 const totalPages = pages.length;
